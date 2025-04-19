d23019131ec1ddfb0d13b554746184a6cd623ab2
pitchaya <106579110+br0wnie25106@users.noreply.github.com>
2025-04-19 14:43:24 +0700

    Add files via upload

---
+++ src:main/DemoBackendApplication.java
@@ -0,0 +1,10 @@
+package com.blackjack;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBackendApplication.class, args);
+    }
+}
