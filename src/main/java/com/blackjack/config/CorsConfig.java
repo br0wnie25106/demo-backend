c6f9549ee38d7b84fc3320f08c584d66c2c7a966
pitchaya <106579110+br0wnie25106@users.noreply.github.com>
2025-04-19 21:31:14 +0700

    Update CorsConfig.java

--- src/main/java/com/blackjack/config/CorsConfig.java
+++ src/main/java/com/blackjack/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://my-blackjackgame.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
